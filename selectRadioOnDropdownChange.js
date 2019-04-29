db1decfed90f09c62ba8b0785f29e9fe3220a870
Martin Lundberg <martin.lundberg@netigate.se>
2019-04-29 16:06:55 +0200

    Change name

--- selectRadioOnDropdownChange.js
+++ selectRadioOnDropdownChange.js
@@ -1,16 +1,16 @@
-function onChange(dropdownId, radioId, bindings){
-    var value = parseInt($(dropdownId + ' option:selected').val())
-    for (var i = 0; i < bindings.length; i++) {
-      var binding = bindings[i]
-      if (value >= binding.from && value <= binding.to) {
-        $($(radioId + ' input')[binding.target]).prop('checked', true)
-      }
+function onSelectRadioOnDropdownChange(dropdownId, radioId, bindings) {
+  var value = parseInt($(dropdownId + ' option:selected').val())
+  for (var i = 0; i < bindings.length; i++) {
+    var binding = bindings[i]
+    if (value >= binding.from && value <= binding.to) {
+      $($(radioId + ' input')[binding.target]).prop('checked', true)
     }
+  }
 }
 
 function selectRadioOnDropdownChange(dropdownId, radioId, bindings) {
   $(dropdownId).change(function() {
-    onChange(dropdownId, radioId, bindings);
+    onSelectRadioOnDropdownChange(dropdownId, radioId, bindings)
   })
 }
 
@@ -21,5 +21,5 @@ function selectRadioOnDropdownChange(dropdownId, radioId, bindings) {
 //         {from: 2, to: 3, target: 1}
 //     ];
 //     selectRadioOnDropdownChange('#ctl21_N-5-124409750', '#ctl22_N-3-124409767', bindings);
-//     onChange('#ctl21_N-5-124409750', '#ctl22_N-3-124409767', bindings);
+//     onSelectRadioOnDropdownChange('#ctl21_N-5-124409750', '#ctl22_N-3-124409767', bindings);
 // })
